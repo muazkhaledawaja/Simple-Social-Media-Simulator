892529e52b693008881d4b647e0c07228271af24
muazkhaledawaja <mkaawaja5000@gmail.com>
2023-04-26 13:47:01 +0200

    modify signup

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -8,9 +8,10 @@ import {
 } from '@nestjs/common';
 
 import * as AWS from 'aws-sdk';
+import * as  crypto from 'crypto';
 
 import { SignupDto, LoginDto } from './dto';
-import { User } from '../../common/types';
+// import { User } from '../../common/types';
 import { ERRORS, PROVIDERS } from '../../common/constants';
 import { generateToken, comparePassword, hashPassword } from '../../common/utils';
 
@@ -46,105 +47,111 @@ export class UserService {
         } if (userNameOrEmail.username) {
             where.userName = userNameOrEmail.username;
         }
-        return this.usersRepository.findOne({where})
+        return this.usersRepository.findOne({ where })
     }
 
     // function to check if user exists by id
     async checkUserById(id: number): Promise<Users> {
-        return this.usersRepository.findOne({ where: {id}})
+        return this.usersRepository.findOne({ where: { id } })
     }
 
 
     // function to create new user in Cognito
-    async signup(user: SignupDto): Promise<User | any> {
+    async signup(user: SignupDto): Promise<any> {
+        const existingUser = await this.checkUser({
+            email: user.email,
+            username: user.username
+        });
+        if (existingUser) {
+            throw new HttpException(ERRORS.USER.ALREADY_EXISTS, HttpStatus.BAD_REQUEST);
+        }
+        const params = {
+            UserPoolId: process.env.AWS_COGNITO_USER_POOL_ID,
+            Username: user.username,
+            TemporaryPassword: user.password,
+            UserAttributes: [
+                {
+                    Name: 'email',
+                    Value: user.email,
+                },
+            ],
+        };
         try {
-            // check if user already exists
-            const checkUserWithEmailOrUsername = await this.checkUser({
-                email: user.email,
-                username: user.username,
-            });
-            if (checkUserWithEmailOrUsername) {
-                throw new HttpException(ERRORS.USER.ALREADY_EXISTS, HttpStatus.BAD_REQUEST);
-            }
-            // create user in Cognito
-            const params = {
-                UserPoolId: process.env.AWS_COGNITO_USER_POOL_ID,
-                Username: user.username,
-                Password: user.password,
-                UserAttributes: [
-                    {
-                        Name: 'email',
-                        Value: user.email,
-                    },
-                ],
-            }
             await this.cognito.adminCreateUser(params).promise();
-
-            // hash password
             const hashedPassword = await hashPassword(user.password, 10);
-            // create user
             const newUser = await this.usersRepository.create({
                 email: user.email,
                 username: user.username,
                 password: hashedPassword,
-                role: user.role
+                role: user.role,
             });
             delete newUser.password;
-            return newUser
-
+            return newUser;
         } catch (error) {
-            throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR)
-
+            throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+
     //function to login user
     async login(loginDetails: LoginDto): Promise<any> {
 
+        const hmac = crypto.createHmac('sha256', process.env.AWS_APP_CLIENT_SECERT);
+        hmac.update(loginDetails.username + process.env.AWS_APP_CLIENT_ID);
+        const secretHash = hmac.digest('base64');
+
+        const params = {
+            AuthFlow: 'USER_PASSWORD_AUTH',
+            ClientId: process.env.AWS_APP_CLIENT_ID,
+            AuthParameters: {
+                EMAIL: loginDetails.email,
+                USERNAME: loginDetails.username,
+                PASSWORD: loginDetails.password,
+                SECRET_HASH: secretHash,
+            },
+        };
+
         try {
-            const params = {
-                AuthFlow: 'USER_PASSWORD_AUTH',
-                ClientId: 'your_app_client_id',
-                UserPoolId: 'your_user_pool_id',
-                AuthParameters: {
-                    USERNAME: loginDetails.username,
-                    PASSWORD: loginDetails.password,
-                },
-            };
-            const { AuthenticationResult } = await this.cognito.initiateAuth(params).promise();
+            const { AuthenticationResult } = await this.cognito.initiateAuth(params).promise() 
+            console.log(888, AuthenticationResult);
 
 
-            // check if user exists
+            // Check if AuthenticationResult and IdToken properties exist
+            if (!AuthenticationResult || !AuthenticationResult.IdToken) {
+                throw new HttpException('Failed to authenticate user 404', HttpStatus.UNAUTHORIZED);
+            }
+
+            // Check if user exists
             const user = await this.usersRepository.findOne({
-                where:
-                {
+                where: {
                     username: loginDetails.username,
                     email: loginDetails.email,
                 },
-            }
-            );
+            });
+
             if (!user) {
                 throw new HttpException(ERRORS.USER.NOT_FOUND, HttpStatus.NOT_FOUND);
             }
-            // check if password is correct
-            const isPasswordCorrect = await comparePassword(
-                loginDetails.password,
-                user.password,
-            );
+
+            // Check if password is correct
+            const isPasswordCorrect = await comparePassword(loginDetails.password, user.password);
+
             if (!isPasswordCorrect) {
                 throw new HttpException(ERRORS.USER.INCORRECT_DATA, HttpStatus.BAD_REQUEST);
             }
-            // delete user password
+
+            // Delete user password
             delete user.password;
+
             return {
                 user: {
                     id: user.id,
                     email: user.email,
-                    username: user.username,
-                    role: user.role
-
+                    username: user.username
                 },
-                token: AuthenticationResult.IdToken,
+                tokens: AuthenticationResult.IdToken
             };
+
         } catch (error) {
             throw new InternalServerErrorException(error)
         }
